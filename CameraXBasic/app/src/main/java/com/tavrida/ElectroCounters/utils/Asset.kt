6d41bc14284912699ec2fed629b4c463060e9b7a
trevol <iamvovan@mail.ru>
2020-08-18 08:52:34 +0300

    delete other versions of assets

--- CameraXBasic/app/src/main/java/com/tavrida/ElectroCounters/utils/Asset.kt
+++ CameraXBasic/app/src/main/java/com/tavrida/ElectroCounters/utils/Asset.kt
@@ -3,6 +3,7 @@ package com.tavrida.ElectroCounters.utils
 import android.content.Context
 import android.os.Environment
 import java.io.*
+import kotlin.jvm.Throws
 
 object Asset {
     /**
@@ -11,8 +12,8 @@ object Asset {
      * @return absolute file path
      */
     @Throws(IOException::class)
-    fun getFilePath(context: Context, assetName: String): String {
-        val file = File(context.filesDir, assetName)
+    fun getFilePath(filesDir: File, context: Context, assetName: String): String {
+        val file = File(filesDir, assetName)
         if (file.exists() && file.length() > 0) {
             return file.absolutePath
         }
@@ -27,17 +28,30 @@ object Asset {
 
     @Throws(IOException::class)
     fun getFilePath(context: Context, assetName: String, deleteOtherVersions: Boolean): String {
-        val filePath = getFilePath(context, assetName)
+        val filePath = getFilePath(context.filesDir, context, assetName)
         if (deleteOtherVersions) {
-            deleteOtherVersions(filePath)
+            deleteOtherVersions(context.filesDir, filePath)
         }
         return filePath
     }
 
-    fun deleteOtherVersions(filePath: String) {
-        val (name, version) = splitNameVersion(filePath)
+    private fun deleteOtherVersions(dir: File, fileName: String) {
+        val fileName = File(fileName).name
+        val (name, version, extension) = splitFilename(fileName)
+        val filesToDelete = dir.listFiles { d, fName ->
+            val del = fName != fileName && fName.endsWith(extension) && fName.startsWith(name)
+            del
+        }
+        filesToDelete?.forEach { it.delete() }
+    }
 
-        TODO()
+    private fun splitFilename(fileName: String): Triple<String, String, String> {
+        val fileName = File(fileName) // aa.6.txt OR aa.txt
+        val fileWithoutExtension = File(fileName.nameWithoutExtension) // aa.6 OR aa
+        val name = fileWithoutExtension.nameWithoutExtension // aa or aa
+        val version = fileWithoutExtension.extension  // 6 or ''
+        val extension = fileName.extension  // txt
+        return Triple(name, version, extension)
     }
 
     fun fileInDownloads(childObj: String = "") = File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), childObj)
